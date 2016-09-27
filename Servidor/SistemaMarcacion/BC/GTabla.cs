da2e308ca83e68b1920ea9e0b700a2e01faf2763
AnthonyLima <a.lima.quispe@isur.edu.pe>
2016-09-26 19:04:20 -0500

    commit 6
    Aumentando Codigo en DA

--- Servidor/SistemaMarcacion/BC/GTabla.cs
+++ Servidor/SistemaMarcacion/BC/GTabla.cs
@@ -5,7 +5,21 @@ using System.Text;
 
 namespace BC
 {
-    class GTabla
+    public class GTabla
     {
+        private string _Tabla;
+        private string _Codigo;
+        private string _Cadena;
+
+        public string Tabla { get { return _Tabla; } set { _Tabla = value; } }
+        public string Codigo { get { return _Codigo; } set { _Codigo = value; } }
+        public string Cadena { get { return _Cadena; } set { _Cadena = value; } }
+
+        public GTabla()
+        {
+            Tabla = string.Empty;
+            Codigo = string.Empty;
+            Cadena = string.Empty;
+        }
     }
 }
---
+++ Servidor/SistemaMarcacion/DA/DAGTabla.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DA
+{
+    public class DAGTabla
+    {
+        private BGlobal.Global Conect = new BGlobal.Global();
+
+        public bool IngresoGTabla(string sTabla,string sCadena)
+        {
+            bool temp = false;
+
+            int Cantidad = 2;
+            string[] Campos = new string[Cantidad];
+            Campos[0] = "Tabla";
+            Campos[1] = "Cadena";
+            object[] objetos = new object[Cantidad];
+            objetos[0] = sTabla;
+            objetos[1] = sCadena;
+
+            temp = Conect.IDU("PITabla", Campos, objetos);
+
+            return temp;
+        }
+
+        public bool ActualizarGTabla(string sTabla,string sCodigo,string sCadena)
+        {
+            bool temp = false;
+
+            int Cantidad = 3;
+            string[] Campos = new string[Cantidad];
+            Campos[0] = "Tabla";
+            Campos[1] = "Codigo";
+            Campos[2] = "Cadena";
+            object[] objetos = new object[Cantidad];
+            objetos[0] = sTabla;
+            objetos[1] = sCodigo;
+            objetos[2] = sCadena;
+
+            temp = Conect.IDU("PUTabla", Campos, objetos);
+
+            return temp;
+        }
+
+        public bool EliminarGTabla(string sTabla, string sCodigo)
+        {
+            bool temp = false;
+
+            int Cantidad = 2;
+            string[] Campos = new string[Cantidad];
+            Campos[0] = "Tabla";
+            Campos[1] = "Codigo";
+            object[] objetos = new object[Cantidad];
+            objetos[0] = sTabla;
+            objetos[1] = sCodigo;
+
+            temp = Conect.IDU("PDTabla", Campos, objetos);
+
+            return temp;
+        }
+    }
+}
