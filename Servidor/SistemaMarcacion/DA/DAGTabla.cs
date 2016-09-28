b4e164a1d8db848f767ac963939ec7a37292c106
AnthonyLima <a.lima.quispe@isur.edu.pe>
2016-09-28 15:25:51 -0500

    commit 12
    Cambios en sistema de nombres de los procesos y cambios en las clases
    del sistema

--- Servidor/SistemaMarcacion/DA/DAGTabla.cs
+++ Servidor/SistemaMarcacion/DA/DAGTabla.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 
 namespace DA
 {
@@ -9,7 +10,22 @@ namespace DA
     {
         private BGlobal.Global Conect = new BGlobal.Global();
 
-        public bool IngresoGTabla(string sTabla,string sCadena)
+        public DataTable PCGTabla(string sTabla)
+        {
+            DataTable dTabla = new DataTable();
+
+            int Cantidad = 1;
+            string[] Campos = new string[Cantidad];
+            Campos[0] = "Tabla";
+            object[] objetos = new object[Cantidad];
+            objetos[0] = sTabla;
+
+            dTabla = Conect.Consulta("MPCGTabla", Campos, objetos);
+
+            return dTabla;
+        }
+
+        public bool PIGTabla(string sTabla,string sCadena)
         {
             bool temp = false;
 
@@ -21,12 +37,12 @@ namespace DA
             objetos[0] = sTabla;
             objetos[1] = sCadena;
 
-            temp = Conect.IDU("PITabla", Campos, objetos);
+            temp = Conect.IDU("MPITabla", Campos, objetos);
 
             return temp;
         }
 
-        public bool ActualizarGTabla(string sTabla,string sCodigo,string sCadena)
+        public bool PUGTabla(string sTabla,string sCodigo,string sCadena)
         {
             bool temp = false;
 
@@ -40,12 +56,12 @@ namespace DA
             objetos[1] = sCodigo;
             objetos[2] = sCadena;
 
-            temp = Conect.IDU("PUTabla", Campos, objetos);
+            temp = Conect.IDU("MPUTabla", Campos, objetos);
 
             return temp;
         }
 
-        public bool EliminarGTabla(string sTabla, string sCodigo = "")
+        public bool PDGTabla(string sTabla, string sCodigo = "")
         {
             bool temp = false;
 
@@ -57,7 +73,7 @@ namespace DA
             objetos[0] = sTabla;
             objetos[1] = sCodigo;
 
-            temp = Conect.IDU("PDTabla", Campos, objetos);
+            temp = Conect.IDU("MPDTabla", Campos, objetos);
 
             return temp;
         }
--- Servidor/SistemaMarcacion/DA/DAVMTrabajador.cs
+++ Servidor/SistemaMarcacion/DA/DAVMTrabajador.cs
@@ -2,12 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 
 namespace DA
 {
     public class DAVMTrabajador
     {
         private BGlobal.Global Conect = new BGlobal.Global();
+
+        public DataTable PCTrabajadorPorCodigoTrabajador(int sCodigoTrabajador)
+        {
+            DataTable dTabla = new DataTable();
+
+            int Cantidad = 1;
+            string[] Campos = new string[Cantidad];
+            Campos[0] = "CodigoTrabajador";
+            object[] objetos = new object[Cantidad];
+            objetos[0] = sCodigoTrabajador;
+
+            dTabla = Conect.Consulta("MPCTrabajadorPorCodigoTrabajador", Campos, objetos);
+
+            return dTabla;
+        }
 
         public bool PITrabajador(BC.VMTrabajador sVMTrabajador)
         {
@@ -30,6 +46,44 @@ namespace DA
             objetos[5] = sVMTrabajador.FechaIngreso;
 
             temp = Conect.IDU("MPITrabajador", Campos, objetos);
+
+            return temp;
+        }
+
+        public bool PUTrabajador(BC.VMTrabajador sVMTrabajador)
+        {
+            bool temp = false;
+
+            int Cantidad = 5;
+            string[] Campos = new string[Cantidad];
+            Campos[0] = "CodigoTrabajador";
+            Campos[1] = "CodigoCategoria";
+            Campos[2] = "CodigoArea";
+            Campos[3] = "CodigoCargo";
+            Campos[4] = "DNI";
+            object[] objetos = new object[Cantidad];
+            objetos[0] = sVMTrabajador.CodigoTrabajador;
+            objetos[1] = sVMTrabajador.CodigoCategoria;
+            objetos[2] = sVMTrabajador.CodigoArea;
+            objetos[3] = sVMTrabajador.CodigoCargo;
+            objetos[4] = sVMTrabajador.DNI;
+
+            temp = Conect.IDU("MPUTrabajador", Campos, objetos);
+
+            return temp;
+        }
+
+        public bool PDTrabajador(int sCodigoTrabajador)
+        {
+            bool temp = false;
+
+            int Cantidad = 1;
+            string[] Campos = new string[Cantidad];
+            Campos[0] = "CodigoTrabajador";
+            object[] objetos = new object[Cantidad];
+            objetos[0] = sCodigoTrabajador;
+
+            temp = Conect.IDU("MPDTrabajador", Campos, objetos);
 
             return temp;
         }
