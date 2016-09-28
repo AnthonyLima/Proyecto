8188adf3b2dfd06e5901bd90368ad28356932b80
AnthonyLima <a.lima.quispe@isur.edu.pe>
2016-09-28 16:58:42 -0500

    commit 17
    Mejorando DA en Permisos

--- Servidor/SistemaMarcacion/DA/DAVMPermiso.cs
+++ Servidor/SistemaMarcacion/DA/DAVMPermiso.cs
@@ -10,6 +10,21 @@ namespace DA
     {
         private BGlobal.Global conect = new BGlobal.Global();
 
+        public DataTable PCPermisoPorCodigoTrabajador(int sCodigoTrabajador)
+        {
+            DataTable dTabla = new DataTable();
+
+            int Cantidad = 1;
+            string[] Campos = new string[Cantidad];
+            Campos[0] = "CodigoTrabajador";
+            object[] Objetos = new object[Cantidad];
+            Objetos[0] = sCodigoTrabajador;
+
+            dTabla = conect.Consulta("MPCPermisoPorCodigoTrabajador", Campos, Objetos);
+
+            return dTabla;
+        }
+
         public bool PIPermiso(BC.VMPermiso sVMPermiso)
         {
             bool temp = false;
@@ -31,6 +46,21 @@ namespace DA
             Objetos[5] = sVMPermiso.Observacion;
 
             temp = conect.IDU("MPIPermiso", Campos, Objetos);
+
+            return temp;
+        }
+
+        public bool PDPermiso(string sCodigoPermiso)
+        {
+            bool temp = false;
+
+            int Cantidad = 1;
+            string[] Campos = new string[Cantidad];
+            Campos[0] = "CodigoPermiso";
+            object[] Objetos = new object[Cantidad];
+            Objetos[0] = sCodigoPermiso;
+
+            temp = conect.IDU("MPDPermiso", Campos, Objetos);
 
             return temp;
         }
